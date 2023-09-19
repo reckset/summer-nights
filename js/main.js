57fe38af4680f4190678a7aaf7550844814635d4
reckset <45181723+reckset@users.noreply.github.com>
2023-09-19 17:23:44 -0600

    Trying to work with weekly tds
    still manually entering weekly on front-end

--- js/main.js
+++ js/main.js
@@ -4,33 +4,31 @@ function TallyTds(){
     var endStr;
 
     fetch(url, {
-    headers: {
-        'Content-Type': 'application/json',
-    }
-    })
-    .then(response => response.json())
-    .then(data => {
-        obj = data;
-    })
-    .then(() => {
-        document.getElementById("dave").innerHTML = obj.Dave;
-        document.getElementById("rex").innerHTML = obj.Rex;
-        document.getElementById("tim").innerHTML = obj.Tim;
-
-        // highlight current leader
-        if ((obj.Dave > obj.Rex) && (obj.Dave > obj.Tim)) {
-        document.getElementById("scoreboard-dave").style.backgroundColor = "green";
-        document.getElementById("scoreboard-dave").style.color = "white";
+        headers: {
+            'Content-Type': 'application/json',
         }
-        else if ((obj.Rex > obj.Dave) && (obj.Rex > obj.Tim)) {
-        document.getElementById("scoreboard-rex").style.backgroundColor = "green";
-        document.getElementById("scoreboard-rex").style.color = "white";
-        }
-        else if ((obj.Tim > obj.Dave) && (obj.Tim > obj.Rex)) {
-        document.getElementById("scoreboard-tim").style.backgroundColor = "green";
-        document.getElementById("scoreboard-tim").style.color = "white";
-        }
-        
-        
-    });
+        })
+        .then(response => response.json())
+        .then(data => {
+            obj = data;
+        })
+        .then(() => {
+            document.getElementById("dave").innerHTML = obj.Dave;
+            document.getElementById("rex").innerHTML = obj.Rex;
+            document.getElementById("tim").innerHTML = obj.Tim;
+    
+            // highlight current leader
+            if ((obj.Dave > obj.Rex) && (obj.Dave > obj.Tim)) {
+            document.getElementById("scoreboard-dave").style.backgroundColor = "green";
+            document.getElementById("scoreboard-dave").style.color = "white";
+            }
+            else if ((obj.Rex > obj.Dave) && (obj.Rex > obj.Tim)) {
+            document.getElementById("scoreboard-rex").style.backgroundColor = "green";
+            document.getElementById("scoreboard-rex").style.color = "white";
+            }
+            else if ((obj.Tim > obj.Dave) && (obj.Tim > obj.Rex)) {
+            document.getElementById("scoreboard-tim").style.backgroundColor = "green";
+            document.getElementById("scoreboard-tim").style.color = "white";
+            }
+        });
 }
