3d33377e7d9192d2131a455e0921112629ca3076
reckset <45181723+reckset@users.noreply.github.com>
2023-11-02 14:09:20 -0600

    Bye week bug
    Fixed bye week bug and override for Week 8

--- js/main.js
+++ js/main.js
@@ -15,14 +15,16 @@ function TallyTds(){
         .then(() => {
             //console.log(obj.TdsByWeek[1]);
             for (var i = 1; i <= obj.TdsByWeek.length; i++) {
-                document.getElementById("week-" + i.toString()).innerHTML = obj.TdsByWeek[i-1];
+                if (i != 6) {
+                    document.getElementById("week-" + i.toString()).innerHTML = obj.TdsByWeek[i-1];
+                }
             }
 
             document.getElementById("dave").innerHTML = obj.Dave;
             document.getElementById("rex").innerHTML = obj.Rex;
             document.getElementById("tim").innerHTML = obj.Tim;
     
-            // highlight current lagger
+            // highlight current lagger orange
             if ((obj.Dave <= obj.Rex) && (obj.Dave <= obj.Tim)) {
             document.getElementById("scoreboard-dave").style.backgroundColor = "orange";
             document.getElementById("scoreboard-dave").style.color = "white";
@@ -35,5 +37,19 @@ function TallyTds(){
             document.getElementById("scoreboard-tim").style.backgroundColor = "orange";
             document.getElementById("scoreboard-tim").style.color = "white";
             }
+
+            // highlight current leader green
+            if ((obj.Dave > obj.Rex) && (obj.Dave > obj.Tim)) {
+                document.getElementById("scoreboard-dave").style.backgroundColor = "green";
+                document.getElementById("scoreboard-dave").style.color = "white";
+                }
+                if ((obj.Rex > obj.Dave) && (obj.Rex > obj.Tim)) {
+                document.getElementById("scoreboard-rex").style.backgroundColor = "green";
+                document.getElementById("scoreboard-rex").style.color = "white";
+                }
+                if ((obj.Tim > obj.Dave) && (obj.Tim > obj.Rex)) {
+                document.getElementById("scoreboard-tim").style.backgroundColor = "green";
+                document.getElementById("scoreboard-tim").style.color = "white";
+                }
         });
 }
